86ceaa023316cb98af75486b8aa6f944bbaf1427
ajgeiss0702 <ajgeiss72@gmail.com>
2021-08-13 11:57:19 -0700

    attempt to always have java 8 compatability

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,11 @@ allprojects {
     version = "2.0.2"
     group = "us.ajg0702"
 
+    plugins.apply("java")
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+    }
+
     tasks.withType<Test>().configureEach {
         useJUnitPlatform()
 
