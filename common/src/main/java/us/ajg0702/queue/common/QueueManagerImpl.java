e8ca809cb07720d30e9279e8a336aaa701b980e0
ajgeiss0702 <ajgeiss72@gmail.com>
2023-10-09 09:07:57 -0700

    Add option to delay queue-servers by up to 3 seconds

--- common/src/main/java/us/ajg0702/queue/common/QueueManagerImpl.java
+++ common/src/main/java/us/ajg0702/queue/common/QueueManagerImpl.java
@@ -470,6 +470,11 @@ public class QueueManagerImpl implements QueueManager {
                         }
                         return;
                     }
+                    long lastSwitch = main.getServerTimeManager().getLastServerChange(player);
+                    int delay = Math.min(Math.max(main.getConfig().getInt("queue-server-delay"), 0), 3000);
+                    if(System.currentTimeMillis() - lastSwitch > delay + 1000 || !player.getCurrentServer().equals(from)) {
+                        return;
+                    }
                     if(
                             !getPlayerQueues(player).contains(to) &&
                                     (
