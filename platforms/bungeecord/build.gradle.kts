30e7de3e9e228dcf330a1256587e816c99f259df
ajgeiss0702 <aj@ajg0702.us>
2025-12-22 17:13:59 -0600

    change to paper repo instead of old velocity repo

--- platforms/bungeecord/build.gradle.kts
+++ platforms/bungeecord/build.gradle.kts
@@ -8,9 +8,9 @@ group = "us.ajg0702.queue.platforms.bungeecord"
 repositories {
     //mavenLocal()
     maven { url = uri("https://repo.ajg0702.us/releases/") }
-    maven { url = uri("https://nexus.velocitypowered.com/repository/maven-public/") }
     maven { url = uri("https://repo.viaversion.com/") }
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
     mavenCentral()
 }
 
